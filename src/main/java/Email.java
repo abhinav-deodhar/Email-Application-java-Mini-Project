08d833bfac33e38cb2f5acb25cef52214a84dba1
ABHINAV DEODHAR <abhinav.deodhar7@gmail.com>
2025-11-28 02:02:19 +0530

    added getter and setter methods.

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -10,26 +12,25 @@ public class Email {
     private double mailboxCapacity;
     private int randomPasswordLength = 10;
     private String alternateEmail;
-    private String companyName="indianarmy";
+    private String companyName="indianarmy.in";
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     // Constructor to receive the first name and last name.
 
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println(
-                "Email Created: " + firstName + " " + lastName);
+        System.out.println("Email Created:");
 
         // Here we call department method that sets the department
         // value
         this.department = setDepartment();
-        System.out.println("Department: " + this.department);
+        //System.out.println("Department: " + this.department);
         this.email=setEmail();
-        System.out.println("Email ID: "+this.email);
-        this.password = randomPassword(
-                randomPasswordLength); // Calling the function by giving
+        //System.out.println("\nYour email Id is: "+this.email);
+        this.password = randomPassword(randomPasswordLength); // Calling the function by giving
         // the length as argument
-        System.out.println("Temporary Password:" + this.password);
+       // System.out.println("\nYour temporary password is:" + this.password);
         // Printing the temporary password.
     }
 
@@ -70,54 +71,74 @@ public class Email {
 //**********************************Department Name**********************************************
     // Ask for the department code and then sets the department value.
     private String setDepartment() {
-        System.out.println("DEPARTMENT CODES"
+        System.out.println(firstName+", Enter the department code"
                 + "\n1 For Sales"
                 + "\n2 For Development"
                 + "\n3 For Accounting"
                 + "\n0 For None"
-                + "\nEnter the department ");
-        Scanner in = new Scanner(System.in);
-        int deptChoice = in.nextInt();
+                + "\nHere -> ");
+
+        int deptChoice =SCANNER.nextInt();
+
         if (deptChoice == 1) {
-            return "Sales";
+            return "Sal";
         } else if (deptChoice == 2) {
-            return "Development";
+            return "Dev";
         } else if (deptChoice == 3) {
-            return "Accounting";
+            return "Acc";
         } else {
             return null;
         }
+
     }
     //**********************************Department Name**********************************************
+
+
+
 
     /***********************************Email Generation******************************************/
 
     private String setEmail(){
         if(department==null || department.equals("NULL")){
-            return firstName+"."+lastName+"@"+companyName.toLowerCase()+".com";
+            return firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+companyName.toLowerCase();
         }else{
-            return firstName+"."+lastName+"@"+department.toLowerCase()+
-                    companyName.toLowerCase()+".com";
+            return firstName.toLowerCase()+"."+lastName.toLowerCase()+"@"+department.toLowerCase()+"."+
+                    companyName.toLowerCase();
 
         }
     }
-
-
-
-
-
     /********************************END HERE Email Generation*************************************/
 
 
-
-
-
-
-
-
-
-
+    /********************************MailBox Capacity*************************************/
     // set the mailbox capacity
+    public void setMailboxCapacity(double capacity){
+        this.mailboxCapacity=capacity;
+    }
 
     // Change the password
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public double getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public String showInfo(){
+        return "\n \nDisplay Name: "+ firstName+" "+lastName+"\n"+
+                "Department: "+department+'\n'+
+                "Company Email: "+email+"\n"+
+                "Temporary Password: "+password+"\n"+
+                "MailBox Capacity:"+mailboxCapacity+" GB";
+    }
+
+    public static void closeScanner(){
+        SCANNER.close();
+}
+
 }
--- src/main/java/EmailApp.java
+++ src/main/java/EmailApp.java
@@ -1,5 +1,9 @@
 public class EmailApp {
     public static void main(String[] args) {
-            Email emi =new Email("John","Smith");
+        Email emi =new Email("Abhinav","Deodhar");
+        emi.setMailboxCapacity(50.00);
+        System.out.println(emi.showInfo());
+        
+        Email.closeScanner();
     }
 }
