0da7777ede9c9ecfcb287c0c49326cc49f574cc8
ABHINAV DEODHAR <abhinav.deodhar7@gmail.com>
2025-11-27 22:04:19 +0530

    Added the Department code

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 
-    public class Email {
+public class Email {
         private String firstName;
         private String lastName;
         private String password;
@@ -13,9 +14,28 @@
         public Email(String firstName, String lastName) {
             this.firstName = firstName;
             this.lastName = lastName;
+            System.out.println("Email Created: "+ firstName+" "+lastName);
+
+            //Here we call department method that sets the department value
+            this.department=setDepartment();
+            System.out.println("Department: "+this.department);
         }
 
-        //Ask for the department
+        //Ask for the department code and then sets the department value.
+        private String setDepartment(){
+            System.out.println("DEPARTMENT CODES\n1 For Sales\n2 For Development\n3 For Accounting\n0 For None\nEnter the department ");
+            Scanner in =new Scanner(System.in);
+            int deptChoice=in.nextInt();
+            if(deptChoice==1){
+                return "Sales";
+            }else if (deptChoice==2){
+                return "Development";
+            }else if (deptChoice==3){
+                return "Accounting";
+            }else {
+                return "Null";
+            }
+        }
 
 
 
