4eb78c34691afed97ea2d2f5ec39298727be6e73
ABHINAV DEODHAR <abhinav.deodhar7@gmail.com>
2025-11-28 00:32:03 +0530

    Added the random password method.

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Email {
@@ -19,11 +20,39 @@ public class Email {
             //Here we call department method that sets the department value
             this.department=setDepartment();
             System.out.println("Department: "+this.department);
+            this.password=randomPassword(10); //Calling the function by giving the length as argument
+            System.out.println("Temporary Password:"+password);
+            //Printing the temporary password.
+
         }
+
+        //Call a method that will return a random password
+    //define a length for the random password
+    private String  randomPassword(int length){
+            String passwordSet="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvxyz01234567890!@#$%^&*()";
+
+        StringBuilder password=new StringBuilder();
+        Random random=new Random();
+
+        for(int i=0;i<length;i++){ //Here it is the length of the password
+             int index= random.nextInt(passwordSet.length()); //here we are taking the random character
+            // from the string and saving its index
+             password.append(passwordSet.charAt(index)); //Here we are appending the character at that index to the password variable.
+        }
+        return password.toString(); //returning the password string converted from string builder.
+
+
+        }
+
 
         //Ask for the department code and then sets the department value.
         private String setDepartment(){
-            System.out.println("DEPARTMENT CODES\n1 For Sales\n2 For Development\n3 For Accounting\n0 For None\nEnter the department ");
+            System.out.println("DEPARTMENT CODES" +
+                    "\n1 For Sales" +
+                    "\n2 For Development" +
+                    "\n3 For Accounting" +
+                    "\n0 For None" +
+                    "\nEnter the department ");
             Scanner in =new Scanner(System.in);
             int deptChoice=in.nextInt();
             if(deptChoice==1){
